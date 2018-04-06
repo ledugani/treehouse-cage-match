c79a632457f35de44e6a3f7d1e9e618d06032572
Tom Dugan <thomas@aadwebb.com>
2018-04-05 20:43:48 -0500

    created buildDomString function

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 const printToDom = (domString, divId) => {
     document.getElementById(divId).innerHTML = domString;
 };
+
+const buildDomString = (player) => {
+    let domString = "";
+    for (let i=0; i < player.length; i++) {
+        domString += `<div id="${player[i].profile_name}">`;
+        domString +=    `<h2>${player[i].name}</h2>`;
+        domString +=    `<img src="${player[i].gravatar_url}">`;
+        domString +=    `<p>${player[i].points.total}</p>`;
+        domString += `</div>`;
+    }
+    printToDom(domString, 'player-card-holder');
+}
